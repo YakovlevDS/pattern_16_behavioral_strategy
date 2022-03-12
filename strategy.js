bc07369e63b884522d4bb8707110a1b696b3c8dd
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-12 19:47:47 +0200

    feat:add example

--- strategy.js
+++ strategy.js
@@ -24,3 +24,17 @@ class AutoCart {
     this.amount = amount;
   }
 };
+
+const baseCustomer = new AutoCart(baseStrategy)
+const premiumCustomer = new AutoCart(premiumStrategy)
+const platinumCustomer = new AutoCart(platinumStrategy)
+
+baseCustomer.setAmount(50000)
+console.log(baseCustomer.checkout());
+
+premiumCustomer.setAmount(50000)
+console.log(premiumCustomer.checkout());
+
+platinumCustomer.setAmount(50000)
+console.log(platinumCustomer.checkout());
+
